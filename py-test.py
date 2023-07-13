6468c79aa2380c0bbe5c07e38e2e6d644fc6fbb9
oscar93varlop <39097708+oscar93varlop@users.noreply.github.com>
2023-07-13 16:29:54 -0500

    Create py-test.py

---
+++ py-test.py
@@ -0,0 +1,7 @@
+with open("resultado.txt", "w") as archivo:
+    for elemento in range(1, 50):
+        if elemento % 2 == 0:
+            mensaje = "El número {} es par.\n".format(elemento)
+        else:
+            mensaje = "El número {} es impar.\n".format(elemento)
+        archivo.write(mensaje)
