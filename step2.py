d2b4b3253cba29bce4df9cdd0a2089f37ee2bff8
oscar93varlop <39097708+oscar93varlop@users.noreply.github.com>
2023-07-15 16:31:57 -0500

    Update step2.py
    sirviendo en ec2

--- step2.py
+++ step2.py
@@ -3,10 +3,13 @@ counter = 0
 try:
   # Using 'x' (exclusive creation) mode will create the file if it doesn't exist.
   # If it exists, it will raise a FileExistsError.
-  with open(filename, 'x') as file:
+  with open(filename, 'w+') as file:
     while counter < 100:
       counter = counter + 1
       text = str(counter)+"\n"
       file.write(text)
     file.close
 #    pass
+except FileExistsError:
+  print(" already exists")
+  print('*' * 20)
