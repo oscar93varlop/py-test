ab7f38791145821cdc8140b2ac55a447175ec379
oscar93varlop <39097708+oscar93varlop@users.noreply.github.com>
2023-07-15 17:44:22 -0500

    Update step2.py

--- step2.py
+++ step2.py
@@ -1,8 +1,9 @@
 # coding=utf-8
 filename = "my_file.txt"
 counter = 0
-with open(filename, 'w') as file:
-  while counter < 100:
-    counter = counter + 1
-    text = str(counter)+"\n"
-    file.write(text)
+with open(filename, 'a') as file:
+    while counter < 100:
+      counter = counter + 1
+      text = str(counter)+"\n"
+      file.write(text)
+#    pass
