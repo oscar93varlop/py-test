29ffd13f9c3ffacd9cb9cee9721432fcb995725d
oscar93varlop <39097708+oscar93varlop@users.noreply.github.com>
2023-07-15 17:43:48 -0500

    Update step2.py

--- step2.py
+++ step2.py
@@ -1,15 +1,8 @@
 # coding=utf-8
 filename = "my_file.txt"
 counter = 0
-try:
-  # Using 'x' (exclusive creation) mode will create the file if it doesn't exist.
-  # If it exists, it will raise a FileExistsError.
-  with open(filename, 'w') as file:
-    while counter < 100:
-      counter = counter + 1
-      text = str(counter)+"\n"
-      file.write(text)
-#    pass
-except FileExistsError:
-  print(" already exists")
-  print('*' * 20)
+with open(filename, 'w') as file:
+  while counter < 100:
+    counter = counter + 1
+    text = str(counter)+"\n"
+    file.write(text)
