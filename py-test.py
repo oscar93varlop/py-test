04f5dc04abdc7facb8ef002c086dd24be59869d1
oscar93varlop <39097708+oscar93varlop@users.noreply.github.com>
2023-07-15 17:33:14 -0500

    Update py-test.py

--- py-test.py
+++ py-test.py
@@ -1,21 +1,6 @@
+# coding=utf-8
 with open("resultado.txt", "w") as archivo:
-    for elemento in range(1, 50):
-        if elemento % 2 == 0:
-            mensaje = "El número {} es par \n".format(elemento)
-        else:
-            mensaje = "El número {} es impar \n".format(elemento)
-        archivo.write(mensaje)
-
-with open("resultado_x.txt", "x") as archivo:
-    for elemento in range(1, 50):
-        if elemento % 2 == 0:
-            mensaje = "El número {} es par \n".format(elemento)
-        else:
-            mensaje = "El número {} es impar \n".format(elemento)
-        archivo.write(mensaje)
-
-with open("resultado_a.txt", "a") as archivo:
-    for elemento in range(1, 50):
+    for elemento in range(1, 51):
         if elemento % 2 == 0:
             mensaje = "El número {} es par \n".format(elemento)
         else:
