4aec2b3f86fd196c13dc44473e2d37f253ed65ba
oscar93varlop <39097708+oscar93varlop@users.noreply.github.com>
2023-07-15 17:36:27 -0500

    Update step2.py

--- step2.py
+++ step2.py
@@ -4,7 +4,7 @@ counter = 0
 try:
   # Using 'x' (exclusive creation) mode will create the file if it doesn't exist.
   # If it exists, it will raise a FileExistsError.
-  with open(filename, 'w+') as file:
+  with open(filename, 'w') as file:
     while counter < 100:
       counter = counter + 1
       text = str(counter)+"\n"
